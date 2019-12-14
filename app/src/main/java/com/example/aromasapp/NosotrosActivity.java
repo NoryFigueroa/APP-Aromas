67f28d75c21fb71a8a4e9a7acb9bbda1ba2678f3
unknown <Admin@IHR80PBF091.continental.edu.pe>
2019-12-14 04:35:44 -0500

    creacion de nosotros y contactenos

---
+++ app/src/main/java/com/example/aromasapp/NosotrosActivity.java
@@ -0,0 +1,14 @@
+package com.example.aromasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NosotrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nosotros);
+    }
+}
